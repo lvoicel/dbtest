15b51c5e9729e86683e5b5538eeb49f2a7a560a4
unknown <lvoicel@ya.ru>
2020-02-10 10:19:28 +0200

    Improved authorization and some bug fixes

--- db.py
+++ db.py
@@ -6,11 +6,12 @@ from PyQt5.QtWidgets import (
     QApplication, QWidget, QPushButton,
     QLabel, QLineEdit, QTextEdit, QGridLayout, QComboBox,
     QFileDialog, QTableWidget, QTableWidgetItem, QHeaderView)
-from PyQt5.QtGui import QPixmap
-from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap, QRegExpValidator
+from PyQt5.QtCore import Qt, QRegExp
 from datetime import datetime
 
 conn = sqlite3.connect("main.db")
+# Connecting to db
 cursor = conn.cursor()
 print("Подключение к базе данных установлено")
 global idR
@@ -27,13 +28,16 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS users
 
 
 class DataB(QWidget):
+    # Main Window
 
-    def __init__(self, userGroup):
+    def __init__(self):
         super(DataB, self).__init__()
-        self.initUI(userGroup)
+        self.initUI()
 
-    def initUI(self, userGroup):
-        self.userGroup = userGroup
+    def initUI(self):
+        user = CurUser
+        # Design of main window
+        self.userGroup = user.userGroup
         self.author = QLabel('Ф.И.О.')
         self.message = QLabel('Сообщение')
         self.option = QLabel('Опция')
@@ -41,6 +45,11 @@ class DataB(QWidget):
         self.imageL = QLabel('Изображение')
         self.authorEdit = QLineEdit(self)
         self.authorEdit.setMaximumSize(340, 30)
+        fio = user.name + " " + user.middleName + " " + user.surName
+        if self.userGroup == ("guest" or "user"):
+            self.authorEdit.setReadOnly(1)
+        self.authorEdit.setText(fio)
+        self.setWindowTitle('DB - ' + fio)
         self.messageEdit = QTextEdit(self)
         self.messageEdit.setMaximumSize(340, 90)
         self.optionDDown = QComboBox()
@@ -72,12 +81,12 @@ class DataB(QWidget):
         self.getBase()
         self.setLayout(grid)
         self.resize(700, 500)
-        self.setWindowTitle('Db')
         self.table.setSortingEnabled(1)
         self.table.setMinimumSize(500, 300)
         print("Оболочка успешно загружена")
 
     def getBase(self):
+        # Creating a QTableWidget
         cursor.execute("SELECT COUNT(*) FROM base")
         self.rows = cursor.fetchone()
         self.table.setRowCount(self.rows[0])
@@ -89,14 +98,17 @@ class DataB(QWidget):
         res = cursor.fetchall()
         i = 0
         j = 0
+        # Adding content in QTable
         if res:
             for i in range(self.rows[0]):
                 for j in range(len(res[i])):
                     if j == 0:
                         item = QTableWidgetItem(str(res[i][j]))
+                        # User group check for permissions
                         if self.userGroup == ("guest" or "user"):
                             item.setFlags(Qt.ItemIsEditable)
                         self.table.setItem(i, j, item)
+                    # Adding an image from db in QTable
                     elif (j == 5) & (res[i][j] != 0):
                         pic = BytesIO(res[i][j])
                         pixmap = QPixmap()
@@ -117,6 +129,7 @@ class DataB(QWidget):
         self.table.resizeRowsToContents()
 
     def delRow(self):
+        # Deliting selected row from QTable and db function
         curr = self.table.currentRow()
         currow = self.table.item(curr, 0).text()
         cursor.execute("DELETE FROM base WHERE num = ?", (currow, ))
@@ -124,6 +137,7 @@ class DataB(QWidget):
         self.table.removeRow(curr)
 
     def sendRes(self):
+        # Sending data in db function
         cursor.execute("SELECT MAX(`num`) FROM base")
         curid = cursor.fetchone()[0]
         if curid is None:
@@ -134,6 +148,7 @@ class DataB(QWidget):
         messageText = self.messageEdit.toPlainText()
         optionText = self.optionDDown.currentText()
         if self.img:
+            # Image blob
             binary = sqlite3.Binary(self.img)
         else:
             binary = 0
@@ -152,6 +167,7 @@ class DataB(QWidget):
         self.getBase()
 
     def getFile(self):
+        # Selecting an image for sending in db
         fname = QFileDialog.getOpenFileName(
             self, 'Open file', 'c:\\', "Image files (*.jpg *.gif *.png *.bmp)")
         if fname:
@@ -171,45 +187,80 @@ class DataB(QWidget):
                     print("Файл не выбран")
 
 
+class User:
+    num = 0
+    name = ""
+    surName = ""
+    middleName = ""
+    userGroup = ""
+    login = ""
+    password = ""
+
+
+class CurUser(User):
+    def __init__(self):
+        super(CurUser, self).__init__()
+
+
 class Auth(QWidget):
+    # Authentication window
 
     def __init__(self):
         super(Auth, self).__init__()
+        # Design of authentication window
         self.setWindowTitle('Авторизация')
+        validator = QRegExp("[А-Яа-яA-Za-z0-9- _]+")
         grid = QGridLayout()
         self.setLayout(grid)
         self.resize(400, 200)
         self.msg = QLabel("")
         self.msg.setAlignment(Qt.AlignCenter)
+        self.msg.setMaximumSize(400, 30)
         self.login = QLineEdit("Логин")
         self.login.setMaximumWidth(200)
+        self.login.setValidator(QRegExpValidator(validator))
+        self.login.setMaxLength(10)
         self.password = QLineEdit("Пароль")
+        self.password.setEchoMode(QLineEdit.Password)
         self.password.setMaximumWidth(200)
+        self.password.setValidator(QRegExpValidator(validator))
+        self.password.setMaxLength(10)
+        self.password.returnPressed.connect(self.authorize)
         self.authBtn = QPushButton('Войти')
+        self.authBtn.setMaximumSize(200, 50)
         grid.setSpacing(2)
         grid.addWidget(self.msg, 1, 1, 1, 3)
-        grid.addWidget(self.login, 2, 1)
-        grid.addWidget(self.password, 2, 3)
-        grid.addWidget(self.authBtn, 3, 1, 3, 3)
+        grid.addWidget(self.login, 2, 2)
+        grid.addWidget(self.password, 3, 2)
+        grid.addWidget(self.authBtn, 4, 2)
         self.authBtn.clicked.connect(self.authorize)
 
     def authorize(self):
-        user = self.login.text()
+        # Creating "User" object from db to log in
+        login = self.login.text()
         password = self.password.text()
-        cursor.execute("SELECT pass FROM users WHERE login = ?", (str(user), ))
+        cursor.execute(
+            "SELECT pass FROM users WHERE login = ?", (str(login), ))
         result = cursor.fetchone()
         if (result[0]) and (result[0] == str(password)):
             cursor.execute(
-                "SELECT usergroup FROM users WHERE login = ?", (str(user), ))
-            authorized = cursor.fetchone()[0]
-            self.showDataB(authorized)
+                "SELECT * FROM users WHERE login = ?", (str(login), ))
+            result = cursor.fetchone()
+            CurUser.num = result[0]
+            CurUser.name = result[1]
+            CurUser.surName = result[2]
+            CurUser.middleName = result[3]
+            CurUser.userGroup = result[4]
+            CurUser.login = result[5]
+            CurUser.password = result[6]
+            self.showDataB()
         if (result[0]) is None or (result[0] != str(password)):
             self.msg.setText("""<h1 style="color: rgb(250, 55, 55);">
                 Неверная пара логин|пароль</h1>""")
-            self.msg.setFont(QFont("Arial", 8, QFont.Bold))
+            self.msg.setFont(QFont("Arial Bold", 6, QFont.Bold))
 
-    def showDataB(self, authorized):
-        self.db = DataB(str(authorized))
+    def showDataB(self):
+        self.db = DataB()
         self.db.show()
         self.close()
 
